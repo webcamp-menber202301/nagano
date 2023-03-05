39e70ba7499bc28c910d8289b43ae156a57b60c9
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2023-03-05 08:49:08 +0000

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :publics, controllers: {
+    sessions: 'public/sessions',
+    passwords: 'publics/passwords',
+    registrations: 'publics/registrations'
+  }
+  devise_for :admins,controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  get "customers/sign_in", to: 'public/sessions#new', as: :new_public_session
+  post "customers/sign_in", to: 'public/sessions#create', as: :public_session
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
